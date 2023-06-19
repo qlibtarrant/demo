016c98287d190b24d434accf252ce451d52ee526
qlibtarrant <btarrant@qualitylogic.com>
2023-06-19 09:59:49 -0400

    Added test cases to pass and fail tests.

--- search_test.js
+++ search_test.js
@@ -3,4 +3,7 @@ Feature('search');
 Scenario('Testing the google homepage',  ({ I }) => {
     I.amOnPage('https://www.google.com');
     I.see('About');
+    I.see('Store');
+    I.see('GmailImages');
+    I.see('Terms');
 });

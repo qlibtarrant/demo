64b06bd51263c15f0e7804cd04046956bf437f58
qlibtarrant <btarrant@qualitylogic.com>
2023-06-21 12:00:44 -0400

    Successfully testing a website dropdown.

--- search_test.js
+++ search_test.js
@@ -3,4 +3,8 @@ Feature('search');
 Scenario('Testing Automation Demo',  ({ I }) => {
     I.amOnPage('http://the-internet.herokuapp.com');
     I.see('Welcome to the-internet');
+    I.click('Dropdown', '#content > ul > li:nth-child(11) > a');
+    I.see('Dropdown List');
+    I.click('#dropdown');
+    I.forceClick('//*[@id="dropdown"]/option[1]');
 });
